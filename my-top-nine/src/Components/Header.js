a4e10451f5bec93d0433371a94edfcb344354511
unknown <jjamal95@gmail.com>
2019-04-24 22:11:18 -0500

    Created Header.js component, usded styled components, and moment.js for live date/time

---
+++ my-top-nine/src/Components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { HeaderH1, HeaderDiv, HeaderSpans } from './Styles';
+import Moment from '../Moment.js/Moment';
+
+
+
+
+const Header = () => {
+  return (
+    <HeaderDiv>
+      <HeaderSpans date> <Moment/></HeaderSpans>
+      <HeaderH1>My Top 9</HeaderH1>
+      <HeaderSpans temp>69°</HeaderSpans>
+        
+    </HeaderDiv>
+
+  )
+}
+
+export default Header
